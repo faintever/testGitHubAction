cddf4ce8c0e9034767d03e7f1717bcf694d7d62a
faintever <faintever@163.com>
2024-09-20 10:33:17 +0800

    hello code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/faintever/testGitHubAction
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	return fmt.Sprintf("Hello, %s!", name)
+}
