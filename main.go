84e7447600e8c86ed24c7e65b040df91c2e204fd
michaelningsh <michael.ning@philips.com>
2024-09-26 10:09:16 +0800

    test

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+
+	return fmt.Sprintf("hello Hello, %s!", name)
 }
